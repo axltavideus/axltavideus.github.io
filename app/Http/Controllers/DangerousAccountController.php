b48804afe27c07c995385ab1bfecb7a57dc50c14
axltavideus <yeetna01@gmail.com>
2025-04-30 22:19:55 +0700

    upload

--- app/Http/Controllers/DangerousAccountController.php
+++ app/Http/Controllers/DangerousAccountController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\DangerousAccount;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class DangerousAccountController extends Controller
 {
@@ -19,20 +20,29 @@ class DangerousAccountController extends Controller
             'bukti_kasus' => 'nullable|file|mimes:jpg,jpeg,png,pdf|max:2048', // max 2MB
         ]);
 
-        $buktiPath = null;
-        if ($request->hasFile('bukti_kasus')) {
-            $buktiPath = $request->file('bukti_kasus')->store('bukti', 'public');
-        }
+        try {
+            $buktiPath = null;
+            if ($request->hasFile('bukti_kasus')) {
+                $file = $request->file('bukti_kasus');
+                $buktiPath = $file->store('bukti', 'public');
+                Log::info('File stored at: ' . $buktiPath);
+            } else {
+                Log::info('No file uploaded.');
+            }
 
-        DangerousAccount::create([
-            'ml_id' => $request->ml_id,
-            'server_id' => $request->server_id,
-            'pelaku_nickname' => $request->pelaku_nickname,
-            'korban_nickname' => $request->korban_nickname,
-            'tanggal_kejadian' => $request->tanggal_kejadian,
-            'bukti_file_path' => $buktiPath,
-            'kronologi' => $request->kronologi,
-        ]);
+            DangerousAccount::create([
+                'ml_id' => $request->ml_id,
+                'server_id' => $request->server_id,
+                'pelaku_nickname' => $request->pelaku_nickname,
+                'korban_nickname' => $request->korban_nickname,
+                'tanggal_kejadian' => $request->tanggal_kejadian,
+                'bukti_file_path' => $buktiPath,
+                'kronologi' => $request->kronologi,
+            ]);
+        } catch (\Exception $e) {
+            Log::error('Error uploading file: ' . $e->getMessage());
+            return redirect()->back()->withErrors(['upload_error' => 'Error uploading file: ' . $e->getMessage()])->withInput();
+        }
 
         return redirect()->back()->with('success', 'Laporan berhasil dikirim!');
     }
--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ENjdO4Dr2bkBIFxQpeoYz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet"/>
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -23,7 +24,7 @@
         <li><a href="#" class="active">Home</a></li>
         <li><a href="{{ url('/topup') }}">Cek ID</a></li>
         <li><a href="#">Kasus</a></li>
-        <li><a href="#">Lapor</a></li>
+        <li><a href="{{ url('/report') }}">Lapor</a></li>
         <li><a href="#">Contacts</a></li>
     </ul>
     <div class="main">
@@ -34,9 +35,11 @@
     </div>
 </header>
 
+<body>
     <main>
         @yield('content')
     </main>
+</body>
 
     <!-- <footer>
     <div class="footer-content">
@@ -85,7 +88,7 @@ body{
     color: var(--text-color);
 }
 header{
-    position: fixed;
+    /* position: fixed; */
     width: 100%;
     top: 0;
     right: 0;
@@ -274,6 +277,7 @@ footer {
     navbar.classList.toggle('open');
 }
     </script>
-    <script src="{ asset('js/app.js') }"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-QF0s0X0x3pbbR6Zb6+6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b" crossorigin="anonymous"></script>
 </body>
 </html>
